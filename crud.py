2dc1605cf0b1fe79d444b09c4d780e25afda928a
Nayeema Sadeque <nstonny@gmail.com>
2026-03-29 23:33:30 +0200

    added the /add routes

--- crud.py
+++ crud.py
@@ -3,3 +3,15 @@ import json
 def get_blog_posts():
     with open("posts.json", "r") as f:
         return json.load(f)
+
+def add_blog_post(title: str, author: str, content: str):
+    posts = get_blog_posts()
+    new_post = {
+                "id": max(post["id"] for post in posts) + 1,
+                "title": title,
+                "author": author,
+                "content": content
+                }
+    posts.append(new_post)
+    with open("posts.json", "w") as f:
+         json.dump(posts, f, indent=4)
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ Development (with auto-reload):
 uvicorn main:app --host 0.0.0.0 --port 5000 --reload
 """
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-from crud import get_blog_posts
+from starlette.responses import RedirectResponse
+
+from crud import get_blog_posts, add_blog_post
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -18,3 +20,17 @@ templates = Jinja2Templates(directory="templates")
 async def index(request: Request):
     blog_posts = get_blog_posts()
     return templates.TemplateResponse(request, 'index.html', context={"posts": blog_posts})
+
+@app.get("/add", response_class=HTMLResponse)
+async def get_form(request: Request):
+    return templates.TemplateResponse(request, 'add.html')
+
+@app.post("/add")
+async def add_post(
+        request:Request,
+        title: str = Form(...),
+        author: str = Form(...),
+        content: str = Form(...) ):
+    add_blog_post(title, author, content)
+    return RedirectResponse(url="/", status_code=303)
+
