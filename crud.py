893e09e9899270984727b02b92c2b771b331c5c1
Nayeema Sadeque <nstonny@gmail.com>
2026-03-31 09:16:42 +0200

    added update route

--- crud.py
+++ crud.py
@@ -4,6 +4,13 @@ def get_blog_posts():
     with open("posts.json", "r") as f:
         return json.load(f)
 
+def get_post_by_id(post_id: int):
+    posts = get_blog_posts()
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    return None
+
 def add_blog_post(title: str, author: str, content: str):
     posts = get_blog_posts()
     new_post = {
@@ -15,3 +22,20 @@ def add_blog_post(title: str, author: str, content: str):
     posts.append(new_post)
     with open("posts.json", "w") as f:
          json.dump(posts, f, indent=4)
+
+def delete_blog_post(post_id: int):
+    posts = get_blog_posts()
+    posts =[post for post in posts if post["id"] != post_id]
+    with open("posts.json", "w") as f:
+        json.dump(posts, f, indent=4)
+
+def update_blog_post(post_id: int, title: str, author: str, content: str):
+    posts = get_blog_posts()
+    for post in posts:
+        if post["id"] == post_id:
+            post["title"] = title
+            post["author"] = author
+            post["content"] = content
+            break
+    with open("posts.json", "w") as f:
+        json.dump(posts, f, indent=4)
--- main.py
+++ main.py
@@ -3,14 +3,13 @@ Development (with auto-reload):
 uvicorn main:app --host 0.0.0.0 --port 5000 --reload
 """
 
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-import uvicorn
 from starlette.responses import RedirectResponse
 
-from crud import get_blog_posts, add_blog_post
+from crud import get_blog_posts, add_blog_post, delete_blog_post, get_post_by_id, update_blog_post
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -33,4 +32,30 @@ async def add_post(
         content: str = Form(...) ):
     add_blog_post(title, author, content)
     return RedirectResponse(url="/", status_code=303)
+
+@app.get("/delete/{post_id}")
+async def delete_post(post_id: int):
+    delete_blog_post(post_id)
+    return RedirectResponse(url="/", status_code=303)
+
+@app.get("/update/{post_id}", response_class=HTMLResponse)
+async def update_form(request: Request, post_id: int):
+    post = get_post_by_id(post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return templates.TemplateResponse(request, 'update.html', context={"post": post})
+
+@app.post("/update/{post_id}")
+async def update_post(post_id: int,
+                      title: str = Form(...),
+                      author: str = Form(...),
+                      content: str = Form(...)):
+    post = get_post_by_id(post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    update_blog_post(post_id, title, author, content)
+    return RedirectResponse(url="/", status_code=303)
+
+
+
 
